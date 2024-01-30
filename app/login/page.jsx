10d7cf2b72bcf3b8c076fa0599b4cd23d0d43a43
zedparman <58415963+zedparman@users.noreply.github.com>
2024-01-30 22:29:33 +0330

    auth done with error

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,4 +1,7 @@
 import LoginForm from '../../components/LoginForm'
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 function Login() {
   return (
--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+"use client";
+
+import {signOut} from 'next-auth-react'
+
+import { useSession } from "next-auth/react"
 
 function UserInfo() {
+    const { data: session } = useSession();
+
   return (
     <div className='grid place-items-center h-screen'>
         <div className='shadow-lg p-8 bg-zince-300/100 flex flex-col gap-2 my-6'>
             <div>
-                Name: <span className='font-bold'>John</span>
+                Name: <span className='font-bold'>{session?.user?.name}</span>
             </div>
             <div>
-                Email: <span className='font-bold'>John@gmail.com</span>
+                Email: <span className='font-bold'>{session?.user?.email}</span>
             </div>
-            <button className='bg-red-500 text-white font-bold px-6 py-2 mt-3'>Log Out</button>
+            <button onClick={() => signOut()} className='bg-red-500 text-white font-bold px-6 py-2 mt-3'>Log Out</button>
         </div>
     </div>
   )
